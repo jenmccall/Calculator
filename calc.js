4c2a85c6a0f018c0c64b630f8e5d7db25d1e94cb
jentern <jennifer.mccall@charter.com>
2018-07-02 11:01:41 -0600

    playing around with new, Apple style calculator

--- calc.js
+++ calc.js
@@ -23,44 +23,63 @@ function addToValue(element, value){
 }
 
 function decimalClick(event) {
-	if (result.querySelector("p").innerHTML.includes('.'))
-		result.querySelector("p").innerHTML;
-	else
-		result.querySelector("p").innerHTML += '.';
+	if (!readValue(result).includes('.'))
+		addToValue(result, '.');
 }
 
 function numberClick(event) {
-	if (result.querySelector("p").innerHTML === '0')
+	if (readValue(result) === '0')
 		setValue(result, readValue(event.currentTarget));
 	else
 		addToValue(result, readValue(event.currentTarget));
+
+	behindTheScenes();
+	behindTheScenes.number(readValue(result));
 }
 
 function operationClick(event) {
-	var operationCheck = result.querySelector("p").innerHTML;
-	var lastChar = operationCheck.charAt(operationCheck.length - 1);
+	var lastChar = readValue(result).charAt(readValue(result).length - 1);
 
-	console.log(lastChar);
+	behindTheScenes();
+	behindTheScenes.operation(readValue(event.currentTarget));
 
+	setValue(result, '0');
+
+	/*
 	if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
-		//if (event.currentTarget.querySelector("p").innerHTML !== lastChar) {
-			result.querySelector("p").innerHTML = readValue(result).substr(0, readValue(result).length - 1);
-			//result.querySelector("p").innerHTML += event.currentTarget.querySelector("p").innerHTML;
-		//}
-
+		setValue(result, readValue(result).substr(0, readValue(result).length - 1));
 	} 
-	result.querySelector("p").innerHTML += event.currentTarget.querySelector("p").innerHTML;
+	addToValue(result, readValue(event.currentTarget));
+	*/
 
 }
 
 function clearScreen(event) {
-	result.querySelector("p").innerHTML = '0';
+	setValue(result, '0');
+}
+
+function behindTheScenes() {
+	var letsCalculate = 0;
+
+
+
+	function operation(nextOperation) {
+		console.log(nextOperation);
+	}
+
+	function number(nextNumber) {
+		letsCalculate += nextNumber;
+		console.log(letsCalculate);
+	}
+
+	behindTheScenes.number = number;
+	behindTheScenes.operation = operation;
 }
 
 function enterClick(event) {
-	var answer = result.querySelector("p").innerHTML;
+	var answer = readValue(result);
 
-	result.querySelector("p").innerHTML = eval(answer);
+	setValue(result, eval(answer));
 }
 
 //for loop setting up event listener for every number button
